9cd1ba8513f25362a24ce4291bc3255140b67e93
Lukasz Dziedziak <lukasz.dziedziak@allegro.pl>
2021-01-14 14:39:38 +0100

    Add registry prefix when building image from dockerfile

--- core/src/main/java/org/testcontainers/images/builder/dockerfile/traits/FromStatementTrait.java
+++ core/src/main/java/org/testcontainers/images/builder/dockerfile/traits/FromStatementTrait.java
@@ -2,12 +2,18 @@ package org.testcontainers.images.builder.dockerfile.traits;
 
 import org.testcontainers.images.builder.dockerfile.statement.SingleArgumentStatement;
 import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.ImageNameSubstitutor;
 
 public interface FromStatementTrait<SELF extends FromStatementTrait<SELF> & DockerfileBuilderTrait<SELF>> {
 
     default SELF from(String dockerImageName) {
-        DockerImageName.parse(dockerImageName).assertValid();
+        DockerImageName parsedImageName = DockerImageName.parse(dockerImageName);
+        parsedImageName.assertValid();
 
-        return ((SELF) this).withStatement(new SingleArgumentStatement("FROM", dockerImageName));
+        String imageNameWithRegistry = ImageNameSubstitutor.instance()
+            .apply(parsedImageName)
+            .asCanonicalNameString();
+
+        return ((SELF) this).withStatement(new SingleArgumentStatement("FROM", imageNameWithRegistry));
     }
 }
